97aa8430ceffdffc99395859e9942b1e09826a7e
r-agus <125457561+r-agus@users.noreply.github.com>
2023-05-09 09:38:26 +0200

    nuevos registros
    Depuración de errores tras ejecución del test global. Creación de los registros 16 y 17

--- BT2/regs/vhdl/regs.vhd
+++ BT2/regs/vhdl/regs.vhd
@@ -20,11 +20,11 @@ port(
     clk :        in std_logic;
     nRst :       in std_logic;
     dato_in_reg :in std_logic_vector(7 downto 0);
-    adr_reg :    buffer std_logic_vector(3 downto 0);
+    adr_reg :    in std_logic_vector(4 downto 0);
     dato_reg:    buffer std_logic_vector(7 downto 0);
-    nWR:         buffer std_logic
+    nWR:         in std_logic;
     ena_in:      in std_logic;
-    ena_out:     buffer std_logic;
+    ena_out:     buffer std_logic
     );
 end entity;
 
@@ -47,6 +47,8 @@ architecture rtl of regs is
     signal reg13 : std_logic_vector(7 downto 0);
     signal reg14 : std_logic_vector(7 downto 0);
     signal reg15 : std_logic_vector(7 downto 0);
+    signal reg16 : std_logic_vector(7 downto 0);
+    signal reg17 : std_logic_vector(7 downto 0);
     
     begin
 
@@ -64,53 +66,65 @@ architecture rtl of regs is
             reg8 <= (others => '0');
             reg9 <= (others => '0');
             reg10 <= (others => '0');
-
+            reg11 <= (others => '0');
+            reg12 <= (others => '0');
+            reg13 <= (others => '0');
+            reg14 <= (others => '0');
+            reg15 <= (others => '0');
+            reg16 <= (others => '0');
+            reg17 <= (others => '0');
+  
             dato_reg <= (others => '0');
             ena_out <= '0';
         
         elsif clk'event and clk='1' then
             if nWR= '0' and ena_in='1' then
                 case adr_reg is
-                    when "0000" => reg0 <= dato_in_reg;
-                    when "0001" => reg1 <= dato_in_reg;
-                    when "0010" => reg2 <= dato_in_reg;
-                    when "0011" => reg3 <= dato_in_reg;
-                    when "0100" => reg4 <= dato_in_reg;
-                    when "0101" => reg5 <= dato_in_reg;
-                    when "0110" => reg6 <= dato_in_reg;
-                    when "0111" => reg7 <= dato_in_reg;
-                    when "1000" => reg8 <= dato_in_reg;
-                    when "1001" => reg9 <= dato_in_reg;
-                    when "1010" => reg10 <= dato_in_reg;
-                    when "1011" => reg11 <= dato_in_reg;
-                    when "1100" => reg12 <= dato_in_reg;
-                    when "1101" => reg13 <= dato_in_reg;
-                    when "1110" => reg14 <= dato_in_reg;
-                    when "1111" => reg15 <= dato_in_reg;
-                    when others => "XX";
+                    when "00000" => reg0 <= dato_in_reg;
+                    when "00001" => reg1 <= dato_in_reg;
+                    when "00010" => reg2 <= dato_in_reg;
+                    when "00011" => reg3 <= dato_in_reg;
+                    when "00100" => reg4 <= dato_in_reg;
+                    when "00101" => reg5 <= dato_in_reg;
+                    when "00110" => reg6 <= dato_in_reg;
+                    when "00111" => reg7 <= dato_in_reg;
+               --     when "1000" => reg8 <= dato_in_reg;
+                    when "01001" => reg9 <= dato_in_reg;
+                    when "01010" => reg10 <= dato_in_reg;
+                    when "01011" => reg11 <= dato_in_reg;
+                    when "01100" => reg12 <= dato_in_reg;
+                    when "01101" => reg13 <= dato_in_reg;
+                    when "01110" => reg14 <= dato_in_reg;
+                    when "01111" => reg15 <= dato_in_reg;
+                    when "10000" => reg16 <= dato_in_reg;
+                    when "10001" => reg17 <= dato_in_reg;
+                    when others => reg8 <= dato_in_reg;
+
                 end case;
                 ena_out <= '1';
 
             elsif nWR='1' and ena_in='1' then  -- es decir, nWR='1'
 
                 case adr_reg is
-                    when "0000" => dato_reg <= reg0;
-                    when "0001" => dato_reg <= reg1;
-                    when "0010" => dato_reg <= reg2;
-                    when "0011" => dato_reg <= reg3;
-                    when "0100" => dato_reg <= reg4;
-                    when "0101" => dato_reg <= reg5;
-                    when "0110" => dato_reg <= reg6;
-                    when "0111" => dato_reg <= reg7;
-                    when "1000" => dato_reg <= reg8;
-                    when "1001" => dato_reg <= reg9;
-                    when "1010" => dato_reg <= reg10;
-                    when "1011" => dato_reg <= reg11;
-                    when "1100" => dato_reg <= reg12;
-                    when "1101" => dato_reg <= reg13;
-                    when "1110" => dato_reg <= reg14;
-                    when "1111" => dato_reg <= reg15;
-                    when others => X"XX";
+                    when "00000" => dato_reg <= reg0;
+                    when "00001" => dato_reg <= reg1;
+                    when "00010" => dato_reg <= reg2;
+                    when "00011" => dato_reg <= reg3;
+                    when "00100" => dato_reg <= reg4;
+                    when "00101" => dato_reg <= reg5;
+                    when "00110" => dato_reg <= reg6;
+                    when "00111" => dato_reg <= reg7;
+                    when "01000" => dato_reg <= reg8;
+                    when "01001" => dato_reg <= reg9;
+                    when "01010" => dato_reg <= reg10;
+                    when "01011" => dato_reg <= reg11;
+                    when "01100" => dato_reg <= reg12;
+                    when "01101" => dato_reg <= reg13;
+                    when "01110" => dato_reg <= reg14;
+                    when "01111" => dato_reg <= reg15;
+                    when "10000" => dato_reg <= reg16;
+                    when "10001" => dato_reg <= reg17;
+                    when others => dato_reg <= "XXXXXXXX";
                 end case;
                 ena_out <= '1';
             else 
